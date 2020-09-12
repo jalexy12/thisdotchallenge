004c3518e418a7bcdbc3b61903add18897f35cc5
Josh Alexy <jalexy12@gmail.com>
2020-09-12 17:55:55 -0500

    Updates

---
+++ precache-manifest.7647f61c26c2f9a564f80347fb7ac136.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c8326c7d3dac67cdef994d3f0959416",
+    "url": "/thisdotchallenge/index.html"
+  },
+  {
+    "revision": "47c09ce31be32ed2d331",
+    "url": "/thisdotchallenge/static/css/main.e76d6218.chunk.css"
+  },
+  {
+    "revision": "8d518566f87f77838b7d",
+    "url": "/thisdotchallenge/static/js/2.121656ca.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/thisdotchallenge/static/js/2.121656ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47c09ce31be32ed2d331",
+    "url": "/thisdotchallenge/static/js/main.75b5fe57.chunk.js"
+  },
+  {
+    "revision": "9cb258e1ebdc9b918f16",
+    "url": "/thisdotchallenge/static/js/runtime-main.795e712d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/thisdotchallenge/static/media/logo.5d5d9eef.svg"
+  }
+]);
